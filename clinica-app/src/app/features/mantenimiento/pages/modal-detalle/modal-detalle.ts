a997d938a1d2622b69300681063d7d5ddc4e816d
ELIAS2401 <tuccieliasluis@gmail.com>
2026-06-09 10:20:42 -0300

    Update de detalle en front
    Ahora tiene opción de ingresar Observación y cancelar la tarea.

--- clinica-app/src/app/features/mantenimiento/pages/modal-detalle/modal-detalle.ts
+++ clinica-app/src/app/features/mantenimiento/pages/modal-detalle/modal-detalle.ts
@@ -1,12 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-modal-detalle',
 
   standalone: true,
 
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './modal-detalle.html',
   styleUrl: './modal-detalle.css',
 })
@@ -16,5 +16,9 @@ export class ModalDetalle {
   @Input() tarea: any;
 
   @Output() cerrar = new EventEmitter<void>();
+  
+  observacion = '';
+
+  readonly maxCaracteres = 150;
 
 }
